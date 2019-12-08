8e99e6dfc91a16937274c19f9cdb509ab85ee1a9
shinsuke-Alex <shinsuke.aragaki1206@gmail.com>
2019-12-08 17:25:11 +0900

    Update range.go

--- range.go
+++ range.go
@@ -3,10 +3,24 @@ package main
 import "fmt"
 
 func main() {
-  ids := []int{32,43,89,600}
+	ids := []int{32, 43, 89, 600}
 
-  // Loop through ids
-  for i, id := range ids {
-	fmt.Printf("%d - ID: %d\n", i, id)
-  }
+	// Loop through ids
+	for i, id := range ids {
+		fmt.Printf("%d - ID: %d\n", i, id)
+	}
+
+	// Add ids together
+	sum := 0
+	for _, id := range ids {
+		sum += id
+	}
+	fmt.Println("Sum", sum)
+
+	// Range with map
+	emails := map[string]string{"Shin": "shin@gmail.com", "Alex": "alex@gmail.com"}
+
+	for k, v := range emails {
+		fmt.Printf("%s: %s\n", k, v)
+	}
 }
